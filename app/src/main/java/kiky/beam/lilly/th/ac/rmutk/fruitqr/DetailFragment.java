7ada504a1c75ec325a6c3f590c245f560a08d4a7
PC <lily-_-123@hotmail.co.th>
2019-02-02 14:03:49 +0700

    Load Image Success

--- app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/DetailFragment.java
+++ app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/DetailFragment.java
@@ -5,10 +5,17 @@ import android.os.Binder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 /**
@@ -16,7 +23,7 @@ import android.widget.TextView;
  */
 public class DetailFragment extends Fragment {
 
-    private  String qrCode;
+    private  String qrCode, nameString, imageString, amountString, unitString, dateString, idShopString, nameShopString, addressString, phoneString;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -39,10 +46,52 @@ public class DetailFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        qrCode = getArguments().getString("QRcode");
+        showView();
 
 
     } //Main Method
+
+    private void showView() {
+        qrCode = getArguments().getString("QRcode");
+        if (!qrCode.isEmpty()) {
+
+//            Have QR codeValue
+            try {
+
+                Myconstant myconstant = new Myconstant();
+                GetDataWhereOneColumn getDataWhereOneColumn = new GetDataWhereOneColumn(getActivity());
+                getDataWhereOneColumn.execute("QR", qrCode, myconstant.getUrlGetDataWhereQR());
+
+                String json = getDataWhereOneColumn.get();
+                Log.d("2FebV1", "json ==> " + json);
+
+                JSONArray jsonArray = new JSONArray(json);
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+                nameString = jsonObject.getString("Name");
+                imageString = jsonObject.getString("Image");
+                idShopString = jsonObject.getString("idUser");
+                amountString = jsonObject.getString("Amount");
+                unitString = jsonObject.getString("Unit");
+
+                TextView nameTextView = getView().findViewById(R.id.txtname);
+                nameTextView.setText(nameString);
+
+                ImageView imageView = getView().findViewById(R.id.imageDetail);
+                Picasso.get().load(imageString).into(imageView);
+
+
+
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
--- app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/GetDataWhereOneColumn.java
+++ app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/GetDataWhereOneColumn.java
@@ -2,17 +2,18 @@ package kiky.beam.lilly.th.ac.rmutk.fruitqr;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.util.Log;
 
+import com.squareup.okhttp.FormEncodingBuilder;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
-public class GetAllDataThread extends AsyncTask<String,Void,String> {
+public class GetDataWhereOneColumn extends AsyncTask<String, Void, String> {
 
     private Context context;
 
-    public GetAllDataThread(Context context) {
+    public GetDataWhereOneColumn(Context context) {
         this.context = context;
     }
 
@@ -20,16 +21,19 @@ public class GetAllDataThread extends AsyncTask<String,Void,String> {
     protected String doInBackground(String... strings) {
 
         try {
+
             OkHttpClient okHttpClient = new OkHttpClient();
+            RequestBody requestBody = new FormEncodingBuilder()
+                    .add("isAdd", "true")
+                    .add(strings[0], strings[1])
+                    .build();
             Request.Builder builder = new Request.Builder();
-            Request request = builder.url(strings[0]).build();
+            Request request = builder.url(strings[2]).post(requestBody).build();
             Response response = okHttpClient.newCall(request).execute();
             return response.body().string();
 
-
-
-        }catch (Exception e){
-            Log.d("6janV1","e="+e.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
             return null;
         }
 
